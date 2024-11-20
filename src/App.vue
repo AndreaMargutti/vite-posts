60385f4801eb12b714fcc1dafa2ad99b3bde4116
AndreaMargutti <marguttiandrea30@gmail.com>
2024-11-20 17:04:43 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script>
+import AppMain from './components/AppMain.vue';
+
+export default {
+  data() {
+    return {
+      
+    }
+  },
+  components: {
+    AppMain,
+  }
+}
+</script>
+
+<template>
+
+<h1>Sono la pagina principale</h1>
+
+</template>
+
+<style lang="scss">
+@use "/node_modules/bootstrap/scss/bootstrap.scss" as *;
+@use "src/general.scss" as *
+</style>
